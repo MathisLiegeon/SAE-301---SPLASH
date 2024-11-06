7598e035fb099290be7fcb6f92076f08ed8fa792
Mathis <150056959+MathisLiegeon@users.noreply.github.com>
2024-11-06 14:36:38 +0100

    lien profile header

--- header.php
+++ header.php
@@ -10,6 +10,35 @@
     <?php wp_head();?>
     <script defer src="<?php echo get_template_directory_uri();?>/assets/js/app.js"></script>
   </head>
+
+  <?php
+  if (is_user_logged_in()) :
+  $current_user= wp_get_current_user();
+  console_log('current_user_');
+  console_log($current_user);
+  $current_user_data = $current_user->data;
+  console_log($current_user_data);
+  $current_user_email = $current_user_data->user_email;
+  console_log($current_user_email);
+
+  $args = array(
+    'post_type' => 'user',
+    'posts_per_page' => -1,
+    'meta_query' => array(
+      array(
+        'key' => 'email',
+        'value' => $current_user_email,
+        'compare' => 'LIKE'
+      )
+    )
+  );
+  $the_query = new WP_Query($args);
+  console_log('the_query');
+  console_log($the_query, true);
+  $user_profile = $the_query->posts[0];
+  console_log($user_profile, true);
+  endif;
+  ?>
   <body>
     <header class="header" id="header">
       <nav class="header-nav">
@@ -19,7 +48,7 @@
       </nav>
     </header>
     <nav class="header-nav-secondary">
-      <a href="">
+      <a href="<?php echo is_user_logged_in() ? get_permalink($user_profile->ID) : '#'?>">
         <img src="<?php echo get_template_directory_uri();?>/assets/icons/icon_person.svg" alt="Icone">
       </a>
       <a href="<?php echo esc_url(home_url());?>">
