5067589bcdab77c4bd4f105eb51c9ccf591b645f
Mathis <150056959+MathisLiegeon@users.noreply.github.com>
2024-11-06 01:23:36 +0100

    footer css

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
 <footer class="footer">
+        <a href="<?php echo esc_url(home_url());?>">
+                <img src="<?php echo get_template_directory_uri();?>/assets/icons/icon_logo.svg" alt="Icone">
+        </a>
         <div class="footer-container">
         <?php
         wp_nav_menu(array('theme_location' => 'footer-menu'));
