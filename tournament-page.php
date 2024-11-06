e61675522439aac446d2ec6925f883b1a7f335e9
Mathis <150056959+MathisLiegeon@users.noreply.github.com>
2024-11-06 09:42:09 +0100

    tournament page php html advencements

--- tournament-page.php
+++ tournament-page.php
@@ -19,31 +19,18 @@ console_log($ID);
 
 $args2 = array (
     'post_type' => 'match',
-    'posts_per_page' => '-1',
+    'posts_per_page' => '4',
+	'meta_query' => array(
+		array(
+			'key' => 'place',
+			'value' => array(1, 2, 3, 4),
+			'compare' => 'IN'
+		)
+	)
 );
-$match = new WP_Query($args2);
+$match_quart = new WP_Query($args2);
 
-if ($match->have_posts()) : $match->the_post();
-console_log($match->posts);
-$match_id = $match->ID;
-console_log('match_id');
-console_log($match_id);
-
-$place = get_post_meta($match_id, 'place', true);
-$team_1 = get_post_meta($match_id, 'team-1', true);
-$team_1_id = $team_1[0];
-$team_2 = get_post_meta($match_id, 'team-2', true);
-$team_2_id = $team_0[0];
-$score_te1 = get_post_meta($match_id, 'score_te1', true);
-$score_te2 = get_post_meta($match_id, 'score_te2', true);
-$date = get_post_meta($match_id, 'start', true);
-$live = get_post_meta($match_id, 'live', true);
-$vod_1 = get_post_meta($match_id, 'vod-1', true);
-$vod_2 = get_post_meta($match_id, 'vod-2', true);
-$vod_3 = get_post_meta($match_id, 'vod-3', true);
-
-console_log('team_1_id');
-console_log($team_1_id);
+if ($match_quart->have_posts()) :
 ?>
 
 <header class="to_header">
@@ -57,44 +44,151 @@ console_log($team_1_id);
 
 <div class="to-container">
     <h3>Bracket</h3>
-    <div class="bracket-container">
-        <div class="braket-col mod-1">
-            <div class="bracket-col-label">Upper Quarterfinals</div>
-            <div class="bracket-row mod-1">
-			<a class="bracket-item"  href="">
-				<div class="bracket-item-team mod-first">
-					<div class="bracket-item-team-name ">
-						<span class="bracket_img">
-							<?php echo $place == 0 ? get_the_post_thumbnail($team_1_id) : ''; ?>
-                        </span>
-						<span><?php echo esc_html($place == 0 ? get_the_title($team_1_id) : ''); ?></span>
-					</div>
-					<div class="bracket-item-team-score">
-						<?php echo esc_html($place == 0 ? get_the_title($score_te1) : ''); ?>
-					</div>
-				</div>
-				<div class="bracket-item-team mod-winner">
-					<div class="bracket-item-team-name">
-						<span class="bracket_img">
-							<?php echo $place == 0 ? get_the_post_thumbnail($team_2_id) : ''; ?>
-                        </span>
-						<span><?php echo esc_html($place == 0 ? get_the_title($team_2_id) : ''); ?></span>
-					</div>
-					<div class="bracket-item-team-score">
-						<?php echo esc_html($place == 0 ? get_the_title($score_te2) : ''); ?>
-					</div>
-				</div>
-				<div class="bracket-item-status moment-tz-convert">
-                    <span><?php echo esc_html($place == 0 ? $date : ''); ?></span>
-                </div>
-				<div class="bracket-item-line mod-down"></div>
-			</a>
-			</div>
-        </div>
+	<div class="to-match-container">
+		<span class="to-text">Quart de finale</span>
+    	<?php
+		foreach ($match_quart->posts as $match) :
 
-        <div class="braket-col mod-2"></div>
-        <div class="braket-col mod-3"></div>
-    </div>
+		console_log($match_quart->posts);
+		console_log('match');
+		console_log($match);
+		$match_id = $match->ID;
+		console_log('match_id');
+		console_log($match_id);
+		$place = get_post_meta($match_id, 'place', true);
+		$team_1 = get_post_meta($match_id, 'team-1', true);
+		$team_1_id = $team_1[0];
+		$team_2 = get_post_meta($match_id, 'team-2', true);
+		$team_2_id = $team_2[0];
+		$score_te1 = get_post_meta($match_id, 'score_te1', true);
+		$score_te2 = get_post_meta($match_id, 'score_te2', true);
+		$date = get_post_meta($match_id, 'start', true);
+
+		console_log('team_1_id');
+		console_log($team_1_id);
+
+		get_template_part('components/match-card', null, array(
+			'match_id' => $match_id,
+			'to_img' => $tournament_id,
+			'te1_name' => get_the_title($team_1[0]),
+			'te1_img' => $team_1[0],
+			'te2_name' => get_the_title($team_2[0]),
+			'te2_img' => $team_2[0],
+			'te1_score' => $score_te1,
+			'te2_score' => $score_te2,
+			'date' => $date
+		));
+		endforeach;
+		?>
+	</div>
+</div>
+<div class="to-container">
+	<div class="to-match-container">
+		<span class="to-text">Demi finale</span>
+    	<?php
+		$demi = array (
+			'post_type' => 'match',
+			'posts_per_page' => '4',
+			'meta_query' => array(
+				array(
+					'key' => 'place',
+					'value' => array(5, 6),
+					'compare' => 'IN'
+				)
+			)
+		);
+		$match_demi = new WP_Query($demi);
+
+		if ($match_demi->have_posts()) :
+		foreach ($match_demi->posts as $match) :
+
+		console_log($match_quart->posts);
+		console_log('match');
+		console_log($match);
+		$match_id = $match->ID;
+		console_log('match_id');
+		console_log($match_id);
+		$place = get_post_meta($match_id, 'place', true);
+		$team_1 = get_post_meta($match_id, 'team-1', true);
+		$team_1_id = $team_1[0];
+		$team_2 = get_post_meta($match_id, 'team-2', true);
+		$team_2_id = $team_2[0];
+		$score_te1 = get_post_meta($match_id, 'score_te1', true);
+		$score_te2 = get_post_meta($match_id, 'score_te2', true);
+		$date = get_post_meta($match_id, 'start', true);
+
+		console_log('team_1_id');
+		console_log($team_1_id);
+
+		get_template_part('components/match-card', null, array(
+			'match_id' => $match_id,
+			'to_img' => $tournament_id,
+			'te1_name' => get_the_title($team_1[0]),
+			'te1_img' => $team_1[0],
+			'te2_name' => get_the_title($team_2[0]),
+			'te2_img' => $team_2[0],
+			'te1_score' => $score_te1,
+			'te2_score' => $score_te2,
+			'date' => $date
+		));
+		endforeach;
+		endif;
+		?>
+	</div>
+</div>
+<div class="to-container">
+	<div class="to-match-container">
+		<span class="to-text">Finale</span>
+    	<?php
+		$final = array (
+			'post_type' => 'match',
+			'posts_per_page' => '4',
+			'meta_query' => array(
+				array(
+					'key' => 'place',
+					'value' => array(7),
+					'compare' => 'IN'
+				)
+			)
+		);
+		$match_final = new WP_Query($final);
+
+		if ($match_final->have_posts()) :
+		foreach ($match_final->posts as $match) :
+
+		console_log($match_quart->posts);
+		console_log('match');
+		console_log($match);
+		$match_id = $match->ID;
+		console_log('match_id');
+		console_log($match_id);
+		$place = get_post_meta($match_id, 'place', true);
+		$team_1 = get_post_meta($match_id, 'team-1', true);
+		$team_1_id = $team_1[0];
+		$team_2 = get_post_meta($match_id, 'team-2', true);
+		$team_2_id = $team_2[0];
+		$score_te1 = get_post_meta($match_id, 'score_te1', true);
+		$score_te2 = get_post_meta($match_id, 'score_te2', true);
+		$date = get_post_meta($match_id, 'start', true);
+
+		console_log('team_1_id');
+		console_log($team_1_id);
+
+		get_template_part('components/match-card', null, array(
+			'match_id' => $match_id,
+			'to_img' => $tournament_id,
+			'te1_name' => get_the_title($team_1[0]),
+			'te1_img' => $team_1[0],
+			'te2_name' => get_the_title($team_2[0]),
+			'te2_img' => $team_2[0],
+			'te1_score' => $score_te1,
+			'te2_score' => $score_te2,
+			'date' => $date
+		));
+		endforeach;
+		endif;
+		?>
+	</div>
 </div>
 
 <?php
