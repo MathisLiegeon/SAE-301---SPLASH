950c121703123b37304d989e945e95ad6486d1e4
Mathis <150056959+MathisLiegeon@users.noreply.github.com>
2024-11-06 13:40:45 +0100

    pahe mentions legales

---
+++ mentions-legales.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Page Mentions legales
+*/
+
+get_header();
+
+if (have_posts()) :
+    while (have_posts()) : the_post();
+?>
+<div class="article-wrapper">
+    <span class="article-thumbnail">
+        <?php echo get_the_post_thumbnail(); ?>
+    </span>
+    <div class="article-content">
+        <?php the_content(); ?>
+    </div>
+</div>
+<?php
+    endwhile;
+endif;
+
+get_footer();
+?>
